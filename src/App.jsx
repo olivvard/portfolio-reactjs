094e5b71b388940758df7667158627d5a09f5b57
olivvard <rahmatrivaldo666@gmail.com>
2025-08-27 22:35:45 +0700

    navbar updates

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,18 @@ import "./App.css";
 import { LoadingScreen } from "./components/loadingScreen";
 import './index.css';
 import { Navbar } from "./components/navbar";
+import { MobileMenu } from "./components/MobileMenu";
 
 function App() {
-  const [isLoaded, setIsLoaded] = useState(false)
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
   return (
     <>
       {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)}/>}{""}
         <div className={`min-h-screen transition-opacity duration-700 ${isLoaded ? "opacity-100" : "opacity-0"} bg-black text-gray-100`}>
           
-          <Navbar/>
+          <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+          <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
 
         </div>
     </>
